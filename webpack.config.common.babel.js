9584921e07cd3495ef3b42d20a1cf2b3cf1dcc1a
Maya Vera <maya@mayavera.me>
2018-12-15 21:48:01 -0500

    created react skeleton

---
+++ webpack.config.common.babel.js
@@ -0,0 +1,48 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const resolve = (...parts) => path.join(__dirname, ...parts)
+
+export default {
+  entry: resolve('src', 'main.js'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ticket Evolution Map',
+      template: resolve('src', 'index.html'),
+      meta: {
+        viewport: 'width=device-width, initial-scale=1'
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              ['@babel/preset-env', {
+                targets: 'last 2 versions'
+              }],
+              '@babel/preset-react'
+            ],
+            plugins: [
+              'babel-plugin-react-require'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }
+      },
+    ]
+  }
+}
